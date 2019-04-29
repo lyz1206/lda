c1318ed053333ce060a66daaad2b4465a40b9548
lyz1206 <42551345+lyz1206@users.noreply.github.com>
2019-04-29 16:31:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lda",
+    version="0.0.1",
+    author="Yizi Lin, Siqi Fu",
+    author_email="yizi.lin@duke.edu",
+    description="Latent Dirichlet Allocation Model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyz1206/lda",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

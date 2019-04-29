5d849bef4cdcecf5714855a01cf162b6ce983053
lyz1206 <42551345+lyz1206@users.noreply.github.com>
2019-04-29 18:35:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lyz1206/lda/ldapkg",
-    packages=setuptools.find_packages(),
+    packages=['ldapkg'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

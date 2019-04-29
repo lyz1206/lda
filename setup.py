f462a6cf8c8916920d51986b68e5a98a6c595702
lyz1206 <42551345+lyz1206@users.noreply.github.com>
2019-04-29 16:32:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lda",
+    name="lda-yizilin",
     version="0.0.1",
     author="Yizi Lin, Siqi Fu",
     author_email="yizi.lin@duke.edu",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/lyz1206/lda",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
